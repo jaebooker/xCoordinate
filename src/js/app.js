7c52174ce93b7f3bd3f0895e558a7d6670d88aa8
jaebooker <jaesonbooker@gmail.com>
2018-10-16 14:05:08 -0700

    added UI, fixed frontend, and UI, now sorting server error

--- src/js/app.js
+++ src/js/app.js
@@ -9,14 +9,14 @@ App = {
       var projectsTemplate = $('#projectsTemplate');
 
       for (i = 0; i < data.length; i ++) {
-        projectTemplate.find('.panel-title').text(data[i].name);
-        projectTemplate.find('img').attr('src', data[i].picture);
-        projectTemplate.find('.project-type').text(data[i].type);
-        projectTemplate.find('.project-date').text(data[i].date);
-        projectTemplate.find('.project-location').text(data[i].location);
-        projectTemplate.find('.btn-join').attr('data-id', data[i].id);
+        projectsTemplate.find('.panel-title').text(data[i].name);
+        projectsTemplate.find('img').attr('src', data[i].picture);
+        projectsTemplate.find('.project-type').text(data[i].type);
+        projectsTemplate.find('.project-date').text(data[i].date);
+        projectsTemplate.find('.project-location').text(data[i].location);
+        projectsTemplate.find('.btn-join').attr('data-id', data[i].id);
 
-        projectsRow.append(projectTemplate.html());
+        projectsRow.append(projectsTemplate.html());
       }
     });
 
@@ -36,26 +36,26 @@ App = {
   },
 
   initContract: function() {
-      $.getJSON('Adoption.json', function(data) {
+      $.getJSON('projects.json', function(data) {
           //get the contract file and instantiate it truffle
           var ProjectsArtifact = data;
-          App.contracts.Projects = TruffleContract(AdoptionArtifact);
+          App.contracts.Projects = TruffleContract(ProjectsArtifact);
           //set provider
           App.contracts.Projects.setProvider(App.web3Provider)
           //use our contract to get and set projects
-          return App.markProjects();
+          return App.markJoin();
       });
       return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '.btn-join', App.handleJoin);
   },
 
-  markProjects: function(projectAdopters, account) {
+  markJoin: function(projectAdopters, account) {
       var projectAdoptionInstance;
       App.contracts.Projects.deployed().then(function(instance) {
-          projectsInstance = instance;
+          projectAdoptionInstance = instance;
 
           return projectAdoptionInstance.getProjectAdopters.call();
       }).then(function(projectAdopters) {
@@ -63,33 +63,31 @@ App = {
               if (projectAdopters[i] !== '0x0000000000000000000000000000000000000000')
               $('.panel-project').eq(i).find('button').text('Success').attr('disabled', true);
           }
-      }
-  }).catch(function(err) {
+      }).catch(function(err) {
       console.log(err.message);
   });
   },
 
   handleJoin: function(event) {
     event.preventDefault();
-    var petId = parseInt($(event.target).data('id'));
+    var projectId = parseInt($(event.target).data('id'));
     var projectAdoptionInstance;
-    web3.eth.getAccounts(function(err, account) {
+    web3.eth.getAccounts(function(err, accounts) {
         if (err) {
             console.log(err);
         }
         var account = accounts[0];
-
         App.contracts.Projects.deployed().then(function(instance) {
             projectAdoptionInstance = instance;
             //use join by sending account
-            return projectAdoptionInstance.join(projectId, {from: account});
+            return projectAdoptionInstance.joinProject(projectId, {from: account});
         }).then(function(result) {
             return App.markJoin();
         }).catch(function(err) {
             console.log(err.message);
         });
     });
-  }
+}//remove paranthesis if not needed
 
 };
 
